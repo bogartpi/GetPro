5dc5855c0c6121de7da1d085bb06feba27001d4f
bogartpi <bogartpimail@gmail.com>
2017-08-30 23:27:55 +1200

    Added LoginController and SignUpController

---
+++ PROCSGO/LogInController.swift
@@ -0,0 +1,18 @@
+//
+//  LogInController.swift
+//  PROCSGO
+//
+//  Created by Pavel Bogart on 30/08/17.
+//  Copyright © 2017 Pavel Bogart. All rights reserved.
+//
+
+import UIKit
+
+class LogInController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
---
+++ PROCSGO/SignUpController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpController.swift
+//  PROCSGO
+//
+//  Created by Pavel Bogart on 30/08/17.
+//  Copyright © 2017 Pavel Bogart. All rights reserved.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white 
+    }
+    
+}
